09753b2d2fcf15648ef48b5ff1dc1c88e8b36217
OnderUmutFiliz <130256502+OnderUmutFiliz@users.noreply.github.com>
2025-01-01 20:37:35 +0300

    Update client.py

--- update/src/python/client.py
+++ update/src/python/client.py
@@ -1,1 +1,14 @@
-/*asdasdasd*/
+import socket
+
+def start_client(port):
+    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as client_socket:
+        client_socket.connect(("localhost", port))
+        print("Bağlantı kuruldu. Komutlarınızı yazın:")
+        while True:
+            message = input("> ")
+            client_socket.send(message.encode())
+            response = client_socket.recv(1024).decode()
+            print(response)
+
+if __name__ == "__main__":
+    start_client(8080)
