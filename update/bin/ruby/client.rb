13bdd9069d56a4f126ce1395320d9b4db4e9629d
berfingokcee <berfin.gokce@bil.omu.edu.tr>
2025-01-01 20:48:37 +0300

    Update client.rb

--- update/bin/ruby/client.rb
+++ update/bin/ruby/client.rb
@@ -1,1 +1,16 @@
-/*asdasdasd*/
+require 'socket'
+
+socket = TCPSocket.new('localhost', 8081)
+puts "Bağlantı kuruldu. Komutlarınızı yazın:"
+
+loop do
+  print "> "
+  message = gets.chomp
+  # Mesajı zorla UTF-8 formatına dönüştür
+  message = message.encode('UTF-8', invalid: :replace, undef: :replace, replace: '?') if message.encoding.name != 'UTF-8'
+  socket.puts message
+  response = socket.gets
+  # Yanıtı zorla UTF-8 formatına dönüştür
+  response = response.encode('UTF-8', invalid: :replace, undef: :replace, replace: '?') if response.encoding.name != 'UTF-8'
+  puts response
+end
